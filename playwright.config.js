53e8384f24147ec465468377af1806bdb35fd9cd
AkshayKathiriya <akshaykathiriya789@gmail.com>
2025-10-20 10:28:18 +0530

    Remove commented baseURL from Playwright configuration

--- playwright.config.js
+++ playwright.config.js
@@ -31,8 +31,7 @@ const config = {
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+
 
     screenshot : "only-on-failure",
     trace : 'on',
