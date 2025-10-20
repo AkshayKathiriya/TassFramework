c4b40507e89b594c75f521d73fed3d8b19e00340
AkshayKathiriya <akshaykathiriya789@gmail.com>
2025-10-20 10:11:24 +0530

    T

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,6 @@ const config = {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot : "only-on-failure",
     trace : 'on',
     video: 'off'
